24062d8914d2f902f03069cabc0d327771fdccb0
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-05-19 09:35:52 +0100

    added few methods

--- prelimaterial/test1.py
+++ prelimaterial/test1.py
@@ -1,2 +1,16 @@
-x = " sjL sdf "
-print(x.lower().strip())
+Index = 0
+self.__ListPositionOfTile = 0
+GridString = self.__CreateTopLine()
+GridLine,Index = self.__CreateEvenLineIndices(Index,True)
+GridString += GridLine
+self.__ListPositionOfTile += 1
+GridLine,Index = self.__CreateOddLineIndices(Index)
+GridString += GridLine
+for count in range (1, self._Size - 1, 2):
+self.__ListPositionOfTile += 1
+GridLine,Index = self.__CreateEvenLineIndices(Index,False)
+GridString += GridLine
+self.__ListPositionOfTile += 1
+GridLine,Index = self.__CreateOddLineIndices(Index)
+GridString += GridLine
+return GridString + self.__CreateBottomLine()
